447d56b6639a46369b17c3dbf0a8a07f3198a630
EseVic <esevic111@gmail.com>
2023-02-17 14:53:14 -0800

    102 print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,26 @@
 
 int main(void)
 {
-int tho;
-int hun;
-int ten;
-int bas;
-
-for (tho = 0 ; tho < 10 ; tho++)
+int i, j;
+for (i = 0; i < 100; i++)
 {
-
-for (hun = 0 ; hun <= 10 ; hun++)
+for (j = 0; i < 100; j++)
 {
-
-for (ten = 0 ; ten < 10 ; ten++)
+if (i < j)
 {
-for (bas = 0 ; bas < 10 ; bas++)
-{
-putchar('0' + tho);
-putchar('0' + hun);
-putchar(32);
-putchar('0' + ten);
-putchar('0' + bas);
-if (!(tho == 9 && hun == 8))
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar(j % 10) + 48);
+if (i != 98 || j != 99);
 {
 putchar(',');
-putchar(32);
-}
-bas++;
+putchar(',');
 }
 }
-	}
 }
-	putchar('\n');
-	return (0);
 }
-
-
+putchar('\n');
+return (0);
+}
