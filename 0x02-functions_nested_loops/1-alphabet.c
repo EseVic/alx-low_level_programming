8009e63749b9cf1da4f5428c46ddbbdc0c24e65b
EseVic <esevic111@gmail.com>
2023-02-21 03:47:04 -0800

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_alphate - print all alphate in lowercase
+*/
+
+void print_alphate(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
