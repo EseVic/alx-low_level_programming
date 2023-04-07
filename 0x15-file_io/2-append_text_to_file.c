0ee68878eac4f687e5ed7150e59b1cfd82486f8c
EseVic <esevic111@gmail.com>
2023-04-07 15:00:01 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,21 +18,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fildes, w, len;
+	int fildes, wt, len;
 
-	fildes = w = len = 0;
-	if (filename == NULL)
+	fildes = wt = len = 0;
+	if (!filename)
 		return (-1);
-	else if (text_content != NULL || !text_content[0])
+
+	else if (!text_content || !text_content[0])
 		return (1);
+
 	fildes = open(filename, O_WRONLY | O_APPEND);
 	if (fildes < 0)
 		return (-1);
+
 	while (text_content[len])
 		len++;
-	w = write(fildes, text_content, len);
-	if (w < 0)
+
+	wt = write(fildes, text_content, len);
+	if (wt < 0)
 		return (-1);
+
 	close(fildes);
 	return (1);
 }
