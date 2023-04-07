67e295ae1fcaea4bcde094eeaca19eed0104db8d
EseVic <esevic111@gmail.com>
2023-04-07 15:58:18 -0700

    file io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,71 +11,113 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void check_IO_stat(int stat, int fd, char *filename, char mode);
+static ssize_t read_file(char *filename, char **buffer, int fildes);
+static void write_copy(char *filename, int fildes, char *buffer, int len);
 
 /**
  * main - this function copies the content of one file to another
- * @argc: argument count
- * @argv: arguments value
+ * @argc: This counts number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 1 on success, exit otherwise
  */
 
 int main(int argc, char *argv[])
 {
-	int src, dest, n_read = 1024, wrote, close_src, close_dest;
-	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	char buffer[1024];
+	int fildes_a, fildes_b, nrd_len, close_des;
+	char *buffer, *file_from, *file_to;
 
-	if (argc != 3) 
+	buffer = NULL;
+	nrd_len = 1;
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	src = open(argv[1], O_RDONLY);
-	check_IO_stat(src, -1, argv[1], 'O');
-	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	check_IO_stat(dest, -1, argv[2], 'W');
-	while (n_read == 1024)
+	file_from = argv[1];
+	file_to = argv[2];
+	fildes_a = open(file_from, O_RDONLY);
+	fildes_b = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (nrd_len > 0)
 	{
-		n_read = read(src, buffer, sizeof(buffer));
-		if (n_read == -1)
-			check_IO_stat(-1, -1, argv[1], 'O');
-		wrote = write(dest, buffer, n_read);
-		if (wrote == -1)
-			check_IO_stat(-1, -1, argv[2], 'W');
+	/* this reads the text from origin file */
+	nrd_len = read_file(file_from, &buffer, fildes_a);
+	if (!nrd_len)
+		break;
+	/* this Writes the buffer to destination */
+	write_copy(file_to, fildes_b, buffer, nrd_len);
 	}
-	close_src = close(src);
-	check_IO_stat(close_src, src, NULL, 'C');
-	close_dest = close(dest);
-	check_IO_stat(close_dest, dest, NULL, 'C');
-	return (0);
-}
 
-/**
- * check_IO_stat - checks if a file can be opened or closed
- * @stat: file descriptor of the file to be opened
- * @filename: name of the file
- * @mode: closing or opening
- * @fd: file descriptor
- *
- * Return: void
- */
-void check_IO_stat(int stat, int fd, char *filename, char mode)
-{
-	if (mode == 'C' && stat == -1)
+	free(buffer);
+	close_des = close(fildes_a);
+	if (close_des < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fildes_a);
 		exit(100);
 	}
-	else if (mode == 'O' && stat == -1)
+	close_des = close(fildes_b);
+	if (close_des < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fildes_b);
+		exit(100);
+	}
+	return (0);
+}
+/**
+ * read_file - A function that read a file into a buffer
+ * @filename: file to read from
+ * @buffer: pointer to a pointer of a buffer.
+ * @fildes: file descriptors
+ *
+ * Return: the current size of the buffer.
+ */
+static ssize_t read_file(char *filename, char **buffer, int fildes)
+{
+	int nrd_len;
+
+	if (fildes < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 		exit(98);
 	}
-	else if (mode == 'W' && stat == -1)
+	if (!(*buffer))
+		*buffer = malloc(sizeof(char) * BUFSIZ);
+	if (!(*buffer))
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	nrd_len = read(fildes, *buffer, BUFSIZ);
+	if (nrd_len < 0)
+	{
+		free(*buffer);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (nrd_len);
+}
+
+/**
+ * write_copy - write the buffer to a file.
+ * @filename: name of the file for contents in buffer
+ * @fildes: file descriptors for the @filename
+ * @buffer: A pointer to a buffer
+ * @len: current size of the buffer.
+ */
+static void write_copy(char *filename, int fildes, char *buffer, int len)
+{
+	if (fildes < 0 || !buffer)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	if (write(fildes, buffer, len) < 0)
+	{
+		free(buffer);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
 		exit(99);
 	}
 }
+
+
