7240cd24a1fe32db3cb4ef0110c093c07916a93a
EseVic <esevic111@gmail.com>
2023-04-07 01:03:54 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,17 +8,18 @@
 #include <stddef.h>
 
 /**
- * create_file - function that will create a new file and fill with with some
- * content.
+ * create_file - create a new file and add some content.
  * @filename: name of the file to create.
- * @text_content: text to add to the new file.
+ * @text_content: content of the text added to the new file.
  *
  * Return: Always 1 on scucess, -1 on Failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int file_des, w, len;
+
+	file_des = w = len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -29,13 +30,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	w = write(file_des, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (file_des == -1 || w == -1)
 		return (-1);
 
-	close(o);
+	close(file_des);
 
 	return (1);
 }
